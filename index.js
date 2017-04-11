ae2bc497728eb15dd109174f467d0de6befd9768
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-04-11 11:09:01 +0200

    structuring into different components.

--- index.js
+++ index.js
@@ -1,59 +1,60 @@
 window.onload = () => {
-    wgl('view', 'fx/2d-vs.fx', 'fx/2d-fs.fx', game)
+    wgl('view', 'fx/2d-vs.fx', 'fx/2d-fs.fx', new Game())
 }
 
-const initialize = (context, program) => {
-    game.objects.push(buildTriangle(context, program))
-}
+function Triangle(vertices) {
+    let _vertices = vertices
 
-const update = (context, program, time) => {
-}
+    this.initialize = (context, program) => {
+    }
 
-const draw = (context, program, time) => {
-    context.clearColor(0.392157, 0.584314, 0.929412, 1.0)
-    // enable depth testing
-    context.enable(context.DEPTH_TEST)
-    // near things obscure far things
-    context.depthFunc(context.LEQUAL)
-    // clear the color as well as the depth buffer
-    context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT)
-    context.viewport(0, 0, context.canvas.width, context.canvas.height)
-    context.useProgram(program)
-}
+    this.update = (context, program, time) => {
+        const position = context.getAttribLocation(program, 'a_position')
+        const buffer = context.createBuffer()
 
-const buildTriangle = (context, program) => {
-    return {
-        update: (context, program, time) => {
-            const position = context.getAttribLocation(program, 'a_position')
-            const buffer = context.createBuffer()
-            // bind the position buffer
-            context.bindBuffer(context.ARRAY_BUFFER, buffer)
-            var positions = [0, 0,
-                0, 0.5,
-                0.7, 0]
-            context.bufferData(context.ARRAY_BUFFER, new Float32Array(positions), context.STATIC_DRAW)
-            context.enableVertexAttribArray(position)
+        // bind the position buffer
+        context.bindBuffer(context.ARRAY_BUFFER, buffer)
+        context.bufferData(context.ARRAY_BUFFER, new Float32Array(_vertices), context.STATIC_DRAW)
+        context.enableVertexAttribArray(position)
 
-            // tell the attribute how to get data out of buffer (ARRAY_BUFFER)
-            const size = 2 // 2 components per iteration
-            const type = context.FLOAT // the data is 32bit floats
-            const normalize = false // don't normalize the data
-            const stride = 0 // 0 = move forward size * sizeof(type) each iteration to get the next position
-            const offset = 0 // start at the beginning of the buffer
-            context.vertexAttribPointer(position, size, type, normalize, stride, offset)
-        },
-        draw: (context, program, time) => {
-            context.drawArrays(context.TRIANGLES, // primitive type
-                0, // offset
-                3) //count
-        }
+        // tell the attribute how to get data out of buffer (ARRAY_BUFFER)
+        context.vertexAttribPointer(position,
+            2, // size: 2 components per iteration
+            context.FLOAT, // type: the data is 32bit floats
+            false, // normalize: don't normalize the data
+            0, // stride: 0 = move forward size * sizeof(type) each iteration to get the next position
+            0) // offset: start at the beginning of the buffer
+    }
+
+    this.draw = (context, program, time) => {
+        context.drawArrays(context.TRIANGLES, // primitive type
+            0, // offset
+            3) //count
     }
 }
 
-const game = {
-    initialize: initialize,
-    update: update,
-    draw: draw,
-    objects: []
+function Game() {
+    this.objects = []
+
+    this.initialize = (context, program) => {
+        this.objects.push(new Triangle([0, 0,
+                                        0, 0.5,
+                                        0.7, 0]))
+    }
+
+    this.update = (context, program, time) => {
+    }
+
+    this.draw = (context, program, time) => {
+        context.clearColor(0.392157, 0.584314, 0.929412, 1.0)
+        // enable depth testing
+        context.enable(context.DEPTH_TEST)
+        // near things obscure far things
+        context.depthFunc(context.LEQUAL)
+        // clear the color as well as the depth buffer
+        context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT)
+        context.viewport(0, 0, context.canvas.width, context.canvas.height)
+        context.useProgram(program)
+    }
 }
 
